f6756f800568beea97648b82eb31137e59037247
omsonawane8080 <omsonawane8080@gmail.in>
2025-02-23 19:29:09 +0530

    Added Spiral Order Matrix and Transpose Matrix programs

---
+++ Lecture11/2.For a given matrix of N x M, print its transpose.java
@@ -0,0 +1,53 @@
+package Lecture11;
+
+import java.util.Scanner;
+
+public class MatrixTranspose {
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] transposeMatrix(int[][] matrix, int rows, int cols) {
+        int[][] transposed = new int[cols][rows]; // Transposed matrix (M x N)
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+        return transposed;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        System.out.println("Enter the matrix elements:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("\nOriginal Matrix:");
+        printMatrix(matrix);
+
+        int[][] transposed = transposeMatrix(matrix, rows, cols);
+
+        System.out.println("\nTransposed Matrix:");
+        printMatrix(transposed);
+
+        sc.close();
+    }
+}
